f30f7c9bad8ba0e83c611fbce1f05c2e3339ef5c
Rafael Marconi Ramos <88451906+rafaelcleversystems@users.noreply.github.com>
2026-04-09 09:51:03 -0300

    Incluindo a resoluçãoi do exercício 1

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int soma=0;
+    int valor;
+    printf 
+    
+    
+      
+    
+    printf(" digite um numero: ");
+    scanf("%d", &num1);
+    
+    
+    soma = num1 + totaldenumeros ;
+    
+    printf(" seu resultado e %d", soma);
+    
+  
+  system("PAUSE");	
+  return 0;
+}
